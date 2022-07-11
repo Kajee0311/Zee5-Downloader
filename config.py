fbf56eade6f6cd51c4958ed65c2169830d0b7f95
Kajee0311 <105401827+Kajee0311@users.noreply.github.com>
2022-07-11 19:03:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+class Config(object):
+
+    # get a token from @BotFather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5360725874:AAGFFWnijtaoC5SOuAshrNyO8dV3RoGNbwU")
+
+    # The Telegram API things
+    # Get these values from my.telegram.org
+    APP_ID = int(os.environ.get("APP_ID", 6309305))
+    API_HASH = os.environ.get("API_HASH", "8b0473750d327598c8585b0049f09e2d")
+
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1757568732").split())
+
+    # Banned Unwanted Members..
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+
+    # Telegram maximum file upload size
+    TG_MAX_FILE_SIZE = 2097152000
+
+    # chunk size that should be used with requests
+    CHUNK_SIZE = 128
+
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
+    
+    # Sql Database url
+    DB_URI = os.environ.get("DATABASE_URL", "")
+    
